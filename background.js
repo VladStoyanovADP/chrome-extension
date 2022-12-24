97f2fec05d0718f6dabcf0e4691655db4cae3d54
Vlad <vladstoyanoff@gmail.com>
2022-12-24 16:05:03 +0000

    urlParams

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+    let uniqueVideoKey = tab.url.split("?")[1]
+    let urlParameters = new URLSearchParams(uniqueVideoKey)
+    console.log(urlParameters)
+
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v")
+    })
+  }
+})

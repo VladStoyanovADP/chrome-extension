e8d8e1d806a9cb2773b6d58bf01c1d905e1c4193
Vlad <vladstoyanoff@gmail.com>
2023-01-21 12:54:58 +0000

    has access to Rumble

--- extension/page.js
+++ extension/page.js
@@ -12,26 +12,27 @@ function CaptureScreenshot() {
 
   var title;
 
-  var headerEls = document.querySelectorAll(
-    "h1.title.ytd-video-primary-info-renderer"
-  );
+//   var headerEls = document.querySelectorAll(
+//     "h1.title.ytd-video-primary-info-renderer"
+//   );
 
-  function SetTitle() {
-    if (headerEls.length > 0) {
-      title = headerEls[0].innerText.trim();
-      return true;
-    } else {
-      return false;
-    }
-  }
+//   function SetTitle() {
+//     if (headerEls.length > 0) {
+//       title = headerEls[0].innerText.trim();
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
 
   if (SetTitle() == false) {
-    headerEls = document.querySelectorAll("h1.watch-title-container");
+    // headerEls = document.querySelectorAll("h1.watch-title-container");
 
     if (SetTitle() == false) title = "";
   }
 
-  var player = document.getElementsByClassName("video-stream")[0];
+  var player = document.getElementsByClassName("videoPlayer-Rumble-cls")[0];
+  console.log(player)
 
   var time = player.currentTime;
 
@@ -92,41 +93,42 @@ function CaptureScreenshot() {
 }
 
 function AddScreenshotButton() {
-  var ytpRightControls =
-    document.getElementsByClassName("ytp-right-controls")[0];
+  var ytpRightControls = document.querySelector(
+    'div[style="width: 100%; height: 30px; margin: 0px; padding: 0px 9px; background: none; opacity: 1; font: 12px sans-serif; box-sizing: border-box; border: none; visibility: visible; text-size-adjust: auto; text-decoration: none; color: inherit; position: absolute; bottom: 0px; display: block; cursor: pointer; z-index: 1010; user-select: none; -webkit-tap-highlight-color: transparent; --darkreader-inline-bgimage:none; --darkreader-inline-bgcolor: initial; --darkreader-inline-border-top: initial; --darkreader-inline-border-right: initial; --darkreader-inline-border-bottom: initial; --darkreader-inline-border-left: initial; --darkreader-inline-color: inherit;"]'
+  );
   if (ytpRightControls) {
     ytpRightControls.prepend(screenshotButton);
   }
 
-  chrome.storage.sync.get("playbackSpeedButtons", function (result) {
-    if (result.playbackSpeedButtons) {
-      ytpRightControls.prepend(speed3xButton);
-      ytpRightControls.prepend(speed25xButton);
-      ytpRightControls.prepend(speed2xButton);
-      ytpRightControls.prepend(speed15xButton);
-      ytpRightControls.prepend(speed1xButton);
+//   chrome.storage.sync.get("playbackSpeedButtons", function (result) {
+//     if (result.playbackSpeedButtons) {
+//       ytpRightControls.prepend(speed3xButton);
+//       ytpRightControls.prepend(speed25xButton);
+//       ytpRightControls.prepend(speed2xButton);
+//       ytpRightControls.prepend(speed15xButton);
+//       ytpRightControls.prepend(speed1xButton);
 
-      var playbackRate = document.getElementsByTagName("video")[0].playbackRate;
-      switch (playbackRate) {
-        case 1:
-          speed1xButton.classList.add("SYTactive");
-          activePBRButton = speed1xButton;
-          break;
-        case 2:
-          speed2xButton.classList.add("SYTactive");
-          activePBRButton = speed2xButton;
-          break;
-        case 2.5:
-          speed25xButton.classList.add("SYTactive");
-          activePBRButton = speed25xButton;
-          break;
-        case 3:
-          speed3xButton.classList.add("SYTactive");
-          activePBRButton = speed3xButton;
-          break;
-      }
-    }
-  });
+//       var playbackRate = document.getElementsByTagName("video")[0].playbackRate;
+//     //   switch (playbackRate) {
+//     //     case 1:
+//     //       speed1xButton.classList.add("SYTactive");
+//     //       activePBRButton = speed1xButton;
+//     //       break;
+//     //     case 2:
+//     //       speed2xButton.classList.add("SYTactive");
+//     //       activePBRButton = speed2xButton;
+//     //       break;
+//     //     case 2.5:
+//     //       speed25xButton.classList.add("SYTactive");
+//     //       activePBRButton = speed25xButton;
+//     //       break;
+//     //     case 3:
+//     //       speed3xButton.classList.add("SYTactive");
+//     //       activePBRButton = speed3xButton;
+//     //       break;
+//     //   }
+//     }
+//   });
 }
 
 var screenshotButton = document.createElement("button");
@@ -136,57 +138,57 @@ screenshotButton.innerHTML = "Screenshot";
 screenshotButton.style.cssFloat = "left";
 screenshotButton.onclick = CaptureScreenshot;
 
-var speed1xButton = document.createElement("button");
-speed1xButton.className = "ytp-button SYText";
-speed1xButton.innerHTML = "1×";
-speed1xButton.onclick = function () {
-  document.getElementsByTagName("video")[0].playbackRate = 1;
-  activePBRButton.classList.remove("SYTactive");
-  this.classList.add("SYTactive");
-  activePBRButton = this;
-};
+// var speed1xButton = document.createElement("button");
+// speed1xButton.className = "ytp-button SYText";
+// speed1xButton.innerHTML = "1×";
+// speed1xButton.onclick = function () {
+//   document.getElementsByTagName("video")[0].playbackRate = 1;
+//   activePBRButton.classList.remove("SYTactive");
+//   this.classList.add("SYTactive");
+//   activePBRButton = this;
+// };
 
-var speed15xButton = document.createElement("button");
-speed15xButton.className = "ytp-button SYText";
-speed15xButton.innerHTML = "1.5×";
-speed15xButton.onclick = function () {
-  document.getElementsByTagName("video")[0].playbackRate = 1.5;
-  activePBRButton.classList.remove("SYTactive");
-  this.classList.add("SYTactive");
-  activePBRButton = this;
-};
+// var speed15xButton = document.createElement("button");
+// speed15xButton.className = "ytp-button SYText";
+// speed15xButton.innerHTML = "1.5×";
+// speed15xButton.onclick = function () {
+//   document.getElementsByTagName("video")[0].playbackRate = 1.5;
+//   activePBRButton.classList.remove("SYTactive");
+//   this.classList.add("SYTactive");
+//   activePBRButton = this;
+// };
 
-var speed2xButton = document.createElement("button");
-speed2xButton.className = "ytp-button SYText";
-speed2xButton.innerHTML = "2×";
-speed2xButton.onclick = function () {
-  document.getElementsByTagName("video")[0].playbackRate = 2;
-  activePBRButton.classList.remove("SYTactive");
-  this.classList.add("SYTactive");
-  activePBRButton = this;
-};
+// var speed2xButton = document.createElement("button");
+// speed2xButton.className = "ytp-button SYText";
+// speed2xButton.innerHTML = "2×";
+// speed2xButton.onclick = function () {
+//   document.getElementsByTagName("video")[0].playbackRate = 2;
+//   activePBRButton.classList.remove("SYTactive");
+//   this.classList.add("SYTactive");
+//   activePBRButton = this;
+// };
 
-var speed25xButton = document.createElement("button");
-speed25xButton.className = "ytp-button SYText";
-speed25xButton.innerHTML = "2.5×";
-speed25xButton.onclick = function () {
-  document.getElementsByTagName("video")[0].playbackRate = 2.5;
-  activePBRButton.classList.remove("SYTactive");
-  this.classList.add("SYTactive");
-  activePBRButton = this;
-};
+// var speed25xButton = document.createElement("button");
+// speed25xButton.className = "ytp-button SYText";
+// speed25xButton.innerHTML = "2.5×";
+// speed25xButton.onclick = function () {
+//   document.getElementsByTagName("video")[0].playbackRate = 2.5;
+//   activePBRButton.classList.remove("SYTactive");
+//   this.classList.add("SYTactive");
+//   activePBRButton = this;
+// };
 
-var speed3xButton = document.createElement("button");
-speed3xButton.className = "ytp-button SYText";
-speed3xButton.innerHTML = "3×";
-speed3xButton.onclick = function () {
-  document.getElementsByTagName("video")[0].playbackRate = 3;
-  activePBRButton.classList.remove("SYTactive");
-  this.classList.add("SYTactive");
-  activePBRButton = this;
-};
+// var speed3xButton = document.createElement("button");
+// speed3xButton.className = "ytp-button SYText";
+// speed3xButton.innerHTML = "3×";
+// speed3xButton.onclick = function () {
+//   document.getElementsByTagName("video")[0].playbackRate = 3;
+//   activePBRButton.classList.remove("SYTactive");
+//   this.classList.add("SYTactive");
+//   activePBRButton = this;
+// };
 
-activePBRButton = speed1xButton;
+// activePBRButton = speed1xButton;
 
 chrome.storage.sync.get(
   [
@@ -218,45 +220,45 @@ chrome.storage.sync.get(
   }
 );
 
-document.addEventListener("keydown", function (e) {
-  if (
-    document.activeElement.contentEditable === "true" ||
-    document.activeElement.tagName === "INPUT" ||
-    document.activeElement.tagName === "TEXTAREA" ||
-    document.activeElement.contentEditable === "plaintext"
-  )
-    return true;
+// document.addEventListener("keydown", function (e) {
+//   if (
+//     document.activeElement.contentEditable === "true" ||
+//     document.activeElement.tagName === "INPUT" ||
+//     document.activeElement.tagName === "TEXTAREA" ||
+//     document.activeElement.contentEditable === "plaintext"
+//   )
+//     return true;
 
-  if (playbackSpeedButtons) {
-    switch (e.key) {
-      case "q":
-        speed1xButton.click();
-        e.preventDefault();
-        return false;
-      case "s":
-        speed15xButton.click();
-        e.preventDefault();
-        return false;
-      case "w":
-        speed2xButton.click();
-        e.preventDefault();
-        return false;
-      case "e":
-        speed25xButton.click();
-        e.preventDefault();
-        return false;
-      case "r":
-        speed3xButton.click();
-        e.preventDefault();
-        return false;
-    }
-  }
+//   if (playbackSpeedButtons) {
+//     switch (e.key) {
+//       case "q":
+//         speed1xButton.click();
+//         e.preventDefault();
+//         return false;
+//       case "s":
+//         speed15xButton.click();
+//         e.preventDefault();
+//         return false;
+//       case "w":
+//         speed2xButton.click();
+//         e.preventDefault();
+//         return false;
+//       case "e":
+//         speed25xButton.click();
+//         e.preventDefault();
+//         return false;
+//       case "r":
+//         speed3xButton.click();
+//         e.preventDefault();
+//         return false;
+//     }
+//   }
 
-  if (screenshotKey && e.key === "p") {
-    CaptureScreenshot();
-    e.preventDefault();
-    return false;
-  }
-});
+//   if (screenshotKey && e.key === "p") {
+//     CaptureScreenshot();
+//     e.preventDefault();
+//     return false;
+//   }
+// });
 
 AddScreenshotButton();
